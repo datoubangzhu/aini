a2e958eb59db4ea1c393c25b2f521873199fa7f2
gmzhao <gmzhao@haiyi-info.com>
2019-03-04 17:05:50 +0800

    springcloud搭建

--- aiwo-power/aiwo-power-app/src/main/java/iknow/u/AiwoApplication.java
+++ aiwo-power/aiwo-power-app/src/main/java/iknow/u/AiwoApplication.java
@@ -2,8 +2,14 @@ package iknow.u;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * @author gmzhao
+ * @date 2019.3.4
+ */
 @SpringBootApplication
+@EnableEurekaServer
 public class AiwoApplication {
 
 	public static void main(String[] args) {
